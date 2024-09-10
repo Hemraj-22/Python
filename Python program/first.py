d4ee1c13caa348fec0080fd27eb4a23ba9600df5
HEM RAJ <160511035+Hemraj-22@users.noreply.github.com>
2024-09-10 21:15:27 +0530

    Add files via upload

---
+++ Python program/first.py
@@ -0,0 +1,49 @@
+# letter=input("Enter the letter:")
+# print(letter)
+# if letter in "aAuUeEoOiI":
+#     print("Vowel")
+# else:
+#     print("Consonants")
+
+# import pandas as pd
+# num=pd.Series([1,2,3,4,5,6])
+# print(num)
+# 
+
+# a,b=2,3
+# txt="@"
+# print(2*txt*3)
+
+# a,b="2",3
+# txt="@"
+# print((a+txt)*3)
+# a,b=1,2
+# print(a//b)
+
+# x,y=12,5
+# print(x//y)
+
+# x,y=-12,5
+# print(x//y)
+
+
+# x,y=12,-5
+# print(x//y)
+
+
+# x,y=-12,6
+# print(x//y)
+
+# a,b=-5,-2
+# print(a%b)
+
+# a,b=-5,-2
+# print(a%b)
+
+
+# num=int(input("enter the number of row:"))
+# for i in range(1,num):
+#     print("*"*i)
+
+
+'hello' and 'world'
